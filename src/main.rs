e98d6b9cb9ba79e9792d443260febca3c6f7d7eb
Jacob Cable <jacobcable94@gmail.com>
2022-01-29 09:14:19 +0000

    fix: lifetimes etc

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
-
-use std::time::{Duration, Instant};
-use std::thread::sleep;
 use device_query::{DeviceQuery, DeviceState, Keycode};
-
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 fn main() {
     run();
 }
 
-const GAME_IS_RUNNING : bool = true;
-const FPS : u128 = 60;
-const SKIP : u128 = 1000/FPS;
+const GAME_IS_RUNNING: bool = true;
+const FPS: u128 = 60;
+const SKIP: u128 = 1000 / FPS;
 
 fn run() {
     let start = Instant::now();
-    let mut frame_counter : u128 = 0;
-    let mut pos : u32 = 4;
-    while(GAME_IS_RUNNING) {
+    let mut frame_counter: u128 = 0;
+    let mut pos: u32 = 4;
+    while (GAME_IS_RUNNING) {
         let now = Instant::now();
         let last_pressed = get_device_state();
-        pos = update_game(pos,last_pressed);
+        pos = update_game(pos, last_pressed);
         display_game(pos);
         frame_counter += SKIP;
         if now.duration_since(start).as_millis() < frame_counter {
@@ -35,7 +33,7 @@ fn get_device_state() -> Vec<Keycode> {
 }
 
 fn update_game(previous_state: u32, last_pressed: Vec<Keycode>) -> u32 {
-    let mut pos : u32 = 0;
+    let mut pos: u32 = 0;
     if last_pressed.contains(&Keycode::LShift) {
         pos = 1
     }
@@ -43,13 +41,14 @@ fn update_game(previous_state: u32, last_pressed: Vec<Keycode>) -> u32 {
     // println!("update time: {}", now.duration_since(start).as_millis());
 }
 
-fn display_game(pos : u32) {
+fn display_game(pos: u32) {
     clear_screen();
     print_grid();
-    // print_border(true, 1, true);
-    // print_body(pos);
-    // print_border(true,1,false);
+    print_border(true, 1, true);
+    print_body(pos);
+    print_border(true, 1, false);
 }
+
 #[derive(Debug)]
 enum CoordState {
     Empty,
@@ -57,18 +56,19 @@ enum CoordState {
     SnekBody,
     Mouse,
 }
+
 #[derive(Debug)]
 struct Coords {
     x: u32,
-    y: u32
+    y: u32,
 }
 
 struct Row {
-    length : u32,
+    length: u32,
     has_snek_head: bool,
     has_mice: bool,
     snek_head_position: u32,
-    mice_positions: Vec<u32>
+    mice_positions: Vec<u32>,
 }
 
 type RowState = Vec<CoordState>;
@@ -78,8 +78,8 @@ type RowState = Vec<CoordState>;
 impl Row {
     fn build_state(&self) -> RowState {
         let mut state = RowState::new();
-        let mut coord_value : CoordState = CoordState::Empty;
         for n in 0..self.length {
+            let mut coord_value: CoordState = CoordState::Empty;
             if self.has_mice {
                 if self.mice_positions.contains(&n) {
                     coord_value = CoordState::Mouse
@@ -96,48 +96,48 @@ impl Row {
     }
     fn display(&self) {
         let state = self.build_state();
-        let to_print = state.iter().map(|s| {
-            match s {
+        let to_print = state
+            .iter()
+            .map(|s| match s {
                 CoordState::Empty => " ",
                 CoordState::Mouse => "M",
                 CoordState::SnekBody => "*",
                 CoordState::SnekHead => "S",
-            }
-        }
-        ).collect().join("");
-        println!("{:#?}",to_print);
+            })
+            .collect::<Vec<_>>()
+            .join("");
+        println!("{:#?}", to_print);
     }
 }
 
 struct Grid {
     width: u32,
     height: u32,
-    rows: Vec<Row>
+    rows: Vec<Row>,
 }
-
 
 enum KinkDirection {
     Left,
-    Right
-} 
+    Right,
+}
 
 struct Snek {
     head_position: Coords,
     length: u32,
-    kinks: Vec<KinkDirection>
+    kinks: Vec<KinkDirection>,
 }
 
 struct Mice {
     population: u32,
-    positions: Vec<Coords>
+    positions: Vec<Coords>,
 }
 
 impl Mice {
-    fn birth (&mut self, pos: Coords) {
+    fn birth(&mut self, pos: Coords) {
         self.population += 1;
         self.positions.push(pos)
     }
-    fn death (&mut self) {
+    fn death(&mut self) {
         self.population -= 1;
         self.positions.pop();
     }
@@ -146,10 +146,9 @@ impl Mice {
 struct GameState {
     snek: Snek,
     score: u32,
-    mice: Mice
+    mice: Mice,
 }
 
-// TODO 2: display rows
 // TODO 3: handle user input for stop/start game
 // TODO 4: handle user input for moving snake head about
 // 5: handle snake eating mouse
@@ -163,19 +162,17 @@ fn print_grid() {
         has_snek_head: false,
         has_mice: false,
         snek_head_position: 0,
-        mice_positions: vec![0]
+        mice_positions: vec![0],
     };
 
     row.display();
 }
 
-
-
 fn clear_screen() {
     print!("\x1B[2J\x1B[1;1H");
 }
 
-fn print_border(empty: bool, _pos: u32,top: bool) {
+fn print_border(empty: bool, _pos: u32, top: bool) {
     if top {
         print!("\n_________________________________________\n");
     }
